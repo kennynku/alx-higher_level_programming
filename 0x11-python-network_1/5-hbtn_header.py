0e1e10c332c5d77428ef474364765b0554c45ac9
kennynku <nkuatsanakeneuoe@gmail.com>
2023-09-01 05:23:04 -0700

    header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-sends a request to the URL and displays the value of the variable X-Request-Id in the response header
+script that sends a request to the URL and displays the value of the variable 
 """
 if __name__ == '__main__':
     import requests
