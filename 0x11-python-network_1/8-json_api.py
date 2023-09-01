6a01f4f8a63c4b701a2e14812e4c408af7dcc705
kennynku <nkuatsanakeneuoe@gmail.com>
2023-09-01 05:51:26 -0700

    done

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+""" sends a POST request to search_user with the letter as a parameter
 """
 
 import requests
