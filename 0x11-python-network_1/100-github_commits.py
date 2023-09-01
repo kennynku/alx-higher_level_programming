73f3b219777d643fef36068659a97c948b520797
kennynku <nkuatsanakeneuoe@gmail.com>
2023-09-01 04:53:04 -0700

    hmm

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,32 +1,12 @@
 #!/usr/bin/python3
 """Listing the 10 commits (from the most recent to oldest) of the repository "rails" by the user "rails"
 """
-import requests
-from requests.auth import HTTPBasicAuth
-from sys import argv
-
-def main(argv):
-    def main(argv):
-        sha = commit_list[i].get('sha')
-        commit = commit_list[i].get('commit')
-        author = commit.get('author')
-        name = author.get('name')
-        print('{}: {}'.format(sha, name))
-
-    repo = argv[1]
-    owner = argv[2]
-    headers = {"Accept": "application/vnd.github.v3+json"}
-    response = requests.get('https://api.github.com/repos/' + owner +
-                            '/' + repo + '/commits', headers=headers)
-    commit_list = response.json()
-    size = len(commit_list)
-    if size < 10:
-        for i in range(0, size):
-            print_commits(i, commit_list)
-    else:
-        for i in range(0, 10):
-            print_commits(i, commit_list)
-
-
-if __name__ == "__main__":
-    main(argv)
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
